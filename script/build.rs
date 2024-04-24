f28ec3b096eaace11265c163c65bb4434bc5c862
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-04-23 17:22:11 -0700

    wip

--- script/build.rs
+++ script/build.rs
@@ -1,7 +1,11 @@
+// use sp1_helper::build_program;
+
 fn main() {
     vergen::EmitBuilder::builder()
         .build_timestamp()
         .git_sha(true)
         .emit()
         .unwrap();
+
+    // build_program("../program")
 }
